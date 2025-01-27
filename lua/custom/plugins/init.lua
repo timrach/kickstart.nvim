b66b2a54acac6152056509de233fc693821ebad3
Tim Rach <tim.rach91@gmail.com>
2025-01-27 08:03:33 +0100

    added autosession

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,7 @@
 -- See the kickstart.nvim README for more information
 vim.keymap.set('n', '<leader>pv', vim.cmd.Oil)
 return {
+  { 'tpope/vim-fugitive' },
   {
     'stevearc/oil.nvim',
     ---@module 'oil'
@@ -13,5 +14,15 @@ return {
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
-  { 'tpope/vim-fugitive' },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Downloads', '/' },
+    },
+  },
 }
